dfab0398d23dd65b59d499774128b246358fadad
AlexisLoya <alexloy117@gmail.com>
2021-01-20 22:36:25 -0600

    Mapper para Category y Product

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,23 +2,37 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository//Indicar a Spring que esta clase se encarga de la base de datos
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
+    public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
     }
 
-    public List<Producto>getByCategoria(int idcategoria){
+    public List<Producto> getByCategoria(int idcategoria) {
         return productoCrudRepository.findByIdCategoriaOrdeOrderByNombreAsc(idcategoria);
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
+    public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
